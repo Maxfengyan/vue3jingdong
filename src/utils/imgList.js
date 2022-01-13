37cddab82a9d811c4640b6fcb719c8012f685e19
Maxma <18624443327@163.com>
2022-01-13 17:58:50 +0800

    基础页面布局

---
+++ src/utils/imgList.js
@@ -0,0 +1,12 @@
+import chaoshi from "../assets/chaoshi.png";
+import caishichang from "../assets/caishichang.png";
+import shuiguodian from "../assets/shuiguodian.png";
+import xianhua from "../assets/xianhua.png";
+import yiyao from "../assets/yiyao.png";
+import jiaju from "../assets/jiaju.png";
+import cake from "../assets/cake.png";
+import qiandao from "../assets/qiandao.png";
+import dapai from "../assets/dapai.png";
+import hongbao from "../assets/hongbao.png";
+// eslint-disable-next-line prettier/prettier
+export default [chaoshi, caishichang, shuiguodian, xianhua, yiyao, jiaju, cake, qiandao, dapai, hongbao];
